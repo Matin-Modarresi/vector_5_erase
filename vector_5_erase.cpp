74ee690b1dd313afe976e743d7b9b07e86afed10
Matin-Modarresi <58595108+Matin-Modarresi@users.noreply.github.com>
2020-09-03 23:07:23 +0430

    Update vector_5_erase.cpp

--- vector_5_erase.cpp
+++ vector_5_erase.cpp
@@ -7,7 +7,16 @@ int main()
 {
 	vector <int> v = {1,2,3,4,5,6,7,8};
 	
+/*1*/	v.erase(v.end()-1);  // 1 2 3 4 5 6 7
+	
+/*2*/	v.erase(v.begin()+2); // 1 2 4 5 6 7
+	
+/*3*/	v.erase(v.begin()+1 , v.begin()+3); // 1 5 6 7
 	
 	for (int i=0 ; i<v.size() ; i++)
-	 cout << v.at(i) << " ";
+	 cout << v.at(i) << " ";  
+	 
+	 cout << "\n\n";
+	 
+
 }
